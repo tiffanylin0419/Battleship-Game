a9ec6279ab32e3e416ae9b5e415b22275555af8e
Tiffany <tl330@duke.edu>
2023-01-25 00:27:53 -0500

    task 1 done

---
+++ app/src/main/java/edu/duke/tl330/battleship/BattleShipBoard.java
@@ -0,0 +1,30 @@
+package edu.duke.tl330.battleship;
+
+public class BattleShipBoard implements Board{
+  private final int width;
+  private final int height;
+
+  public int getWidth(){
+    return width;
+  }
+  public int getHeight(){
+    return height;
+  }
+  
+  BattleShipBoard(int w,int h){
+   /**
+   * Constructs a BattleShipBoard with the specified width and height
+   * @param w is the width of the newly constructed board.
+   * @param h is the height of the newly constructed board.
+   * @throws IllegalArgumentException if the width or height are less than or equal to zero.
+   */
+    if (w <= 0) {
+      throw new IllegalArgumentException("BattleShipBoard's width must be positive but is " + w);
+    }
+    if (h <= 0) {
+      throw new IllegalArgumentException("BattleShipBoard's height must be positive but is " + h);
+    }
+    this.width=w;
+    this.height=h;
+  }
+}
---
+++ app/src/main/java/edu/duke/tl330/battleship/Board.java
@@ -0,0 +1,6 @@
+package edu.duke.tl330.battleship;
+
+public interface Board {
+  public int getWidth();
+  public int getHeight();
+}
