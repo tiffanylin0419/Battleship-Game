42ee646558b80f320fa0927925744f4fe67ed46b
Tiffany <tl330@duke.edu>
2023-01-25 19:23:45 -0500

    task 6 done

---
+++ app/src/main/java/edu/duke/tl330/battleship/BasicShip.java
@@ -0,0 +1,41 @@
+package edu.duke.tl330.battleship;
+
+public class BasicShip  implements Ship<Character> {
+  private final Coordinate myLocation;
+
+  public BasicShip(Coordinate location){
+    this.myLocation=location;
+  }
+  
+  @Override
+  public boolean occupiesCoordinates(Coordinate where) {
+    // TODO Auto-generated method stub
+    return where.equals(myLocation);
+  }
+
+  @Override
+  public boolean isSunk() {
+    // TODO Auto-generated method stub
+    return false;
+  }
+
+  @Override
+  public void recordHitAt(Coordinate where) {
+    // TODO Auto-generated method stub
+    
+  }
+
+  @Override
+  public boolean wasHitAt(Coordinate where) {
+    // TODO Auto-generated method stub
+    return false;
+  }
+
+  @Override
+  public Character getDisplayInfoAt(Coordinate where) {
+    // TODO Auto-generated method stub
+    return 's';
+  }
+  
+
+}
